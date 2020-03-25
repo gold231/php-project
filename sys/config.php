355409cc8ed71ec0eb949eaab2490945e2b99711
GoldDragon <53452201+goldenfox55@users.noreply.github.com>
2020-03-26 05:54:42 +0700

    social media

---
+++ sys/config.php
@@ -0,0 +1,26 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://pixelphotoscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: pixelphotoscript@gmail.com   
+// +------------------------------------------------------------------------+
+// | Pixel Photo Script
+// | Copyright (c) 2018 pixelphoto. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "bancomvp.mysql.dbaas.com.br";
+// MySQL Database User
+$sql_db_user = "bancomvp";
+// MySQL Database Password
+$sql_db_pass = "doacao@2020";
+// MySQL Database Name
+$sql_db_name = "bancomvp";
+
+// Site URL
+$site_url = "http://collactionapp.com/beta"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "e728eae0-f1b9-41de-8afc-5820dc30da20"; // Your purchase code, don't give it to anyone. 
+$buyer = "diego_rodrigues"; // Your buyer name from envato. 
+?>
